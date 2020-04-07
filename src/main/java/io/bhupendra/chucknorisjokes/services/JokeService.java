5497dd644b7b06e59534f62907c05cdbe4f2ee88
bhupendra <bhupendrapt95@gmail.com>
2020-04-07 14:05:15 +0530

    Added jokeService and JokeServiceImpl

---
+++ src/main/java/io/bhupendra/chucknorisjokes/services/JokeService.java
@@ -0,0 +1,7 @@
+package io.bhupendra.chucknorisjokes.services;
+
+public interface JokeService {
+
+    String sayJoke();
+}
+
---
+++ src/main/java/io/bhupendra/chucknorisjokes/services/JokeServiceImpl.java
@@ -0,0 +1,20 @@
+package io.bhupendra.chucknorisjokes.services;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JokeServiceImpl implements JokeService {
+
+
+    private final ChuckNorrisQuotes chuckNorrisQuotes;
+
+    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+    }
+
+    @Override
+    public String sayJoke() {
+        return chuckNorrisQuotes.getRandomQuote();
+    }
+}
